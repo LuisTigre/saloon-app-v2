68b9adc0e9da1307e1e8fc57e554c55a21578d13
LuisTigre <luistigre0@gmail.com>
2024-11-23 02:25:05 +0100

    feat: booking page table

--- src/app/pages/bookings/bookings.component.ts
+++ src/app/pages/bookings/bookings.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { PagerComponent } from "../../components/pager/pager.component";
 import { CommonModule } from '@angular/common';
-
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-bookings',
-
   standalone: true,
-  imports: [PagerComponent, CommonModule],
+  imports: [PagerComponent, CommonModule, MatIcon],
   templateUrl: './bookings.component.html',
-  styleUrl: './bookings.component.scss'
+  styleUrls: ['./bookings.component.scss']
 })
 export class BookingsComponent {
   menu: string[] = [
@@ -18,5 +17,68 @@ export class BookingsComponent {
     'To Pay',
     'Canceled',
     'To be Rated',
+  ];
+
+  bookings = [
+    {
+      date: "February 14",
+      time: "13:15",
+      service: {
+        name: "Butter-fly locs",
+        length: "long",
+        color: "brow",
+        style: "curly"
+      },
+      status: "Please pay the booking fee",
+      amount: "PLN 700.00"
+    },
+    {
+      date: "February 14",
+      time: "13:15",
+      service: {
+        name: "Butter-fly locs",
+        length: "long",
+        color: "brow",
+        style: "curly"
+      },
+      status: "All Confirmed, see you soon",
+      amount: "PLN 700.00"
+    },
+    {
+      date: "February 14",
+      time: "13:15",
+      service: {
+        name: "Butter-fly locs",
+        length: "long",
+        color: "brow",
+        style: "curly"
+      },
+      status: "Please complete the payment",
+      amount: "PLN 700.00"
+    },
+    {
+      date: "February 14",
+      time: "13:15",
+      service: {
+        name: "Butter-fly locs",
+        length: "long",
+        color: "brow",
+        style: "curly"
+      },
+      status: "Please rate the service",
+      amount: "PLN 700.00"
+    },
+    {
+      date: "February 14",
+      time: "13:15",
+      service: {
+        name: "Butter-fly locs",
+        length: "long",
+        color: "brow",
+        style: "curly"
+      },
+      status: "Thank you, come back soon",
+      amount: "PLN 700.00"
+    }
   ];
 }
