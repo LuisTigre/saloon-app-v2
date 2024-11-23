3c88fc3567e3fe6e446e60b7b9da8b4a17243059
LuisTigre <luistigre0@gmail.com>
2024-11-23 03:14:32 +0100

    fix: size and width

--- src/app/pages/bookings/bookings.component.ts
+++ src/app/pages/bookings/bookings.component.ts
@@ -26,59 +26,120 @@ export class BookingsComponent {
       service: {
         name: "Butter-fly locs",
         length: "long",
-        color: "brow",
+        color: "brown",
         style: "curly"
       },
       status: "Please pay the booking fee",
       amount: "PLN 700.00"
     },
     {
-      date: "February 14",
-      time: "13:15",
+      date: "February 15",
+      time: "10:30",
       service: {
-        name: "Butter-fly locs",
-        length: "long",
-        color: "brow",
-        style: "curly"
+        name: "Box braids",
+        length: "medium",
+        color: "black",
+        style: "straight"
       },
       status: "All Confirmed, see you soon",
-      amount: "PLN 700.00"
+      amount: "PLN 500.00"
     },
     {
-      date: "February 14",
-      time: "13:15",
+      date: "February 16",
+      time: "09:00",
       service: {
-        name: "Butter-fly locs",
-        length: "long",
-        color: "brow",
-        style: "curly"
+        name: "Cornrows",
+        length: "short",
+        color: "blonde",
+        style: "wavy"
       },
       status: "Please complete the payment",
-      amount: "PLN 700.00"
+      amount: "PLN 350.00"
     },
     {
-      date: "February 14",
-      time: "13:15",
+      date: "February 17",
+      time: "11:45",
       service: {
-        name: "Butter-fly locs",
+        name: "Twists",
         length: "long",
-        color: "brow",
+        color: "red",
         style: "curly"
       },
       status: "Please rate the service",
-      amount: "PLN 700.00"
+      amount: "PLN 600.00"
     },
     {
-      date: "February 14",
-      time: "13:15",
+      date: "February 18",
+      time: "14:00",
       service: {
-        name: "Butter-fly locs",
-        length: "long",
-        color: "brow",
-        style: "curly"
+        name: "Loc extensions",
+        length: "extra long",
+        color: "black",
+        style: "straight"
       },
       status: "Thank you, come back soon",
-      amount: "PLN 700.00"
+      amount: "PLN 800.00"
+    },
+    {
+      date: "February 19",
+      time: "15:30",
+      service: {
+        name: "Knotless braids",
+        length: "medium",
+        color: "brown",
+        style: "wavy"
+      },
+      status: "Please complete the payment",
+      amount: "PLN 550.00"
+    },
+    {
+      date: "February 20",
+      time: "12:00",
+      service: {
+        name: "Faux locs",
+        length: "long",
+        color: "auburn",
+        style: "curly"
+      },
+      status: "All Confirmed, see you soon",
+      amount: "PLN 750.00"
+    },
+    {
+      date: "February 21",
+      time: "16:45",
+      service: {
+        name: "Passion twists",
+        length: "short",
+        color: "dark brown",
+        style: "straight"
+      },
+      status: "Thank you, come back soon",
+      amount: "PLN 450.00"
+    },
+    {
+      date: "February 22",
+      time: "10:15",
+      service: {
+        name: "Crochet braids",
+        length: "medium",
+        color: "blonde",
+        style: "wavy"
+      },
+      status: "Please rate the service",
+      amount: "PLN 600.00"
+    },
+    {
+      date: "February 23",
+      time: "13:30",
+      service: {
+        name: "Feed-in braids",
+        length: "long",
+        color: "black",
+        style: "straight"
+      },
+      status: "Please pay the booking fee",
+      amount: "PLN 500.00"
     }
   ];
+  
 }
