38ed48206235a5f4b1eeb7ab280a82e906114a83
LuisTigre <luistigre0@gmail.com>
2025-03-06 13:47:43 +0100

    Reapply "feat: create and hairstyle btn and db save"
    This reverts commit db2307880bddbca1d9b30bc03551c6ccb2fb3b2f.

--- src/app/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/catalog.component.ts
@@ -1,37 +1,134 @@
 import { Component, OnInit } from '@angular/core';
-import { PagerComponent } from "../../components/pager/pager.component";
+import { PagerComponent } from '../../components/pager/pager.component';
 import { SearchboxComponent } from '../../components/searchbox/searchbox.component';
-import { CardComponent } from "../../components/card/card.component";
-import { Card } from "../../components/card/card.component";
+import { CardComponent } from '../../components/card/card.component';
+import { Card } from '../../components/card/card.component';
 import { BraidingStylesService } from '../../services/braiding-style.service';
+import { AddButtonComponent } from '../../components/svg/add-button/add-button.component';
+import { EventDialogComponent } from '../../components/event-dialog/event-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [PagerComponent, SearchboxComponent, CardComponent],
+  imports: [
+    PagerComponent,
+    SearchboxComponent,
+    CardComponent,
+    AddButtonComponent,
+    EventDialogComponent,
+  ],
   providers: [BraidingStylesService],
   templateUrl: './catalog.component.html',
-  styleUrl: './catalog.component.scss'
+  styleUrl: './catalog.component.scss',
 })
 export class CatalogComponent implements OnInit {
-  
-  def_img: string = "https://media.istockphoto.com/id/2186821055/pl/zdj%C4%99cie/kobieta-postawi%C5%82a-nowoczesne-akcesoria-do-w%C5%82os%C3%B3w.jpg?s=612x612&w=0&k=20&c=0K96UKxNQ3xQmTps3bPddDFT_Eji-LgC9VZSJeoOKuo=";
+  def_img: string =
+    "https://images.unsplash.com/photo-1600456899121-68eda5705257?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z3JheXxlbnwwfHwwfHx8MA%3D%3D";
   cards: Card[] = [];
 
-  constructor(private braidingStylesService: BraidingStylesService) {}
+  constructor(
+    private braidingStylesService: BraidingStylesService,
+    private dialog: MatDialog,
+    private toastService: ToastrService
+  ) {}
 
   ngOnInit(): void {
+    this.updateServiceList();
+  }
+
+  updateServiceList():void{
     this.braidingStylesService.getBraidingStyles().subscribe({
       next: (response: any[]) => {
+
         console.log('Braiding styles fetched:', response);
         this.cards = response.map((style: any) => ({
           id: style.id,
-          img: style.images && style.images.length > 0 ? style.images[0].image_url : this.def_img,
-          title: style.style_name
+          img:
+            style.images && style?.images[0]?.image_url != null || ''
+              ? style.images[0].image_url
+              : this.def_img,
+          title: style.name,
         }));
       },
-      error: (error: any) => console.error('Failed to fetch braiding styles:', error),
+      error: (error: any) =>
+        console.error('Failed to fetch braiding styles:', error),
+    });
+  }
+
+  openModal(): void {
+    const dialogRef = this.dialog.open(EventDialogComponent, {
+      data: {
+        title: 'Create Hairstyle',
+        dynamicFields: [
+          {
+            type: 'text',
+            label: 'Name',
+            name: 'name',
+            placeholder: 'Enter the name',
+            value: '',
+            // validators: [Validators.required]
+          },
+          {
+            type: 'textarea',
+            label: 'Description',
+            name: 'description',
+            placeholder: 'Describe the hairstyle',
+            value: '',
+            // validators: [Validators.required]
+          },
+          {
+            type: 'text',
+            label: 'Duration',
+            name: 'duration',
+            placeholder: 'Set the avg time it takes',
+            value: '',
+            // validators: [Validators.required]
+          },
+          {
+            type: 'text',
+            label: 'Price',
+            name: 'price',
+            placeholder: 'How much it will cost',
+            value: '',
+            // validators: [Validators.required]
+          },
+        ],
+
+        buttons: [
+          {
+            text: 'Create',
+            value: false,
+            action: () => {
+              dialogRef.close();
+              const formGroup = dialogRef.componentInstance.formGroup;
+              if (formGroup.valid) {
+                dialogRef.close({ ...formGroup.value, register: true });
+              }
+            },
+          },
+        ],
+      },
+    });
+    dialogRef.afterClosed().subscribe((result: any) => {
+      console.log('Dialog result:', result);
+      if (!result) {
+        console.log('Dialog was closed without submission.');
+        return;
+      }
+
+      const { name, description, duration, price } = result;
+      this.braidingStylesService
+        .createBraindingsStyle(name, description, duration, price)
+        .subscribe({
+          next: (response: any) =>{
+            this.toastService.success('Hairstyle created:', response)
+            this.updateServiceList();
+          },
+          error: (response: any) =>
+            this.toastService.error('Creation failed:', response),
+        });
     });
   }
 }
-
--- src/app/services/braiding-style.service.ts
+++ src/app/services/braiding-style.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
@@ -13,7 +13,7 @@ type Image = {
 };
 
 export type BraidingStyleResponse = {
-  id: number; 
+  id: number;
   title: string;
   description: string;
   price: number;
@@ -32,7 +32,7 @@ type StyleAttribute = {
   values: StyleAttributeValue[];
 };
 
-type Hairstyle = {
+export type Hairstyle = {
   id: number;
   title: string;
   description: string;
@@ -40,30 +40,138 @@ type Hairstyle = {
   stylesAtributes: StyleAttribute[];
   duration: number;
   price: string;
-  editable: boolean;
 };
+
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class BraidingStylesService {
-  private apiUrl: string = `http://${window.location.hostname || 'localhost'}:8000/api/braiding-styles`;
+  private apiUrl: string = `http://${
+    window.location.hostname || 'localhost'
+  }:8000/api`;
 
   constructor(private httpClient: HttpClient) {}
 
   // Fetch all braiding styles
   getBraidingStyles(): Observable<BraidingStyleResponse[]> {
-    return this.httpClient.get<any[]>(this.apiUrl);      
+    return this.httpClient.get<any[]>(`${this.apiUrl}/hairstyles`);
   }
 
   // Fetch braiding style details
   getBraidingStyleDetail(id: string): Observable<Hairstyle> {
-    return this.httpClient.get<Hairstyle>(`${this.apiUrl}/${id}/showDetails`);
+    return this.httpClient.get<Hairstyle>(`${this.apiUrl}/hairstyles/${id}/showDetails`);
+  }
+   // Fetch braiding style details
+   getAllAttributes(): Observable<Hairstyle> {
+    return this.httpClient.get<Hairstyle>(`${this.apiUrl}/hairstyle-attributes`);
+  }
+   // Fetch braiding style details
+   getAttributeValuesByHairstyleId(id: Number): Observable<Hairstyle> {
+    return this.httpClient.get<any>(`${this.apiUrl}/hairstyles-attributes/${id}/values`);
   }
 
+  createBraindingsStyle(
+    name: string,
+    description: string,
+    duration: string,
+    price: string
+  ): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${sessionStorage.getItem('auth-token')}`,
+    });
+    return this.httpClient.post<any>(
+      `${this.apiUrl}/hairstyles`,
+      { name, description, duration, price },
+      { headers }
+    );
+  } 
+
+  updateBraindingsStyle(
+    name: string,
+    description: string,
+    duration: string,
+    price: string
+  ): Observable<any> {
+    const urlParams = new URLSearchParams(window.location.search);
+    const braid_id = urlParams.get('brast');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${sessionStorage.getItem('auth-token')}`,
+    });
+    return this.httpClient.put<any>(
+      `${this.apiUrl}/hairstyles/${braid_id}`,
+      { name, description, duration, price },
+      { headers }
+    );
+  }
   
+  createBraindingsAttribute(
+    attribute_name: string,
+    attribute_value: string,
+  ): Observable<any> {
+    const urlParams = new URLSearchParams(window.location.search);
+    const braid_id = urlParams.get('brast');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${sessionStorage.getItem('auth-token')}`,
+    });
+    return this.httpClient.post<any>(
+      `${this.apiUrl}/${braid_id}/attributes`,
+      { attribute_name, attribute_value },
+      { headers }
+    );
+  }
+
+  attachHairAttribute(
+    hairstyle_id: number,
+    hairstyle_attribute_value_id: number,
+    additional_cost: number,
+    cost_type: string
+  ): Observable<any> {
+    const urlParams = new URLSearchParams(window.location.search);
+    const braid_id = urlParams.get('brast');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${sessionStorage.getItem('auth-token')}`,
+    });
+    return this.httpClient.post<any>(
+      `${this.apiUrl}/hairstyle-attribute-pricing`,
+      { hairstyle_id, hairstyle_attribute_value_id, additional_cost, cost_type },
+      { headers }
+    ); 
+  }
   
-  
+  updateAttachedHairAttribute(
+    att_price_id: number,    
+    additional_cost: number,
+    cost_type: string
+  ): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${sessionStorage.getItem('auth-token')}`,
+    });
+    return this.httpClient.put<any>(
+      `${this.apiUrl}/hairstyle-attribute-pricing/${att_price_id}`,
+      { additional_cost, cost_type },
+      { headers }
+    );
+  }
+
+  deleteAttachedHairAttribute(
+    att_price_id: number
+  ): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${sessionStorage.getItem('auth-token')}`,
+    });
+    return this.httpClient.delete<any>(
+      `${this.apiUrl}/hairstyle-attribute-pricing/${att_price_id}`,
+      { headers }
+    );
+  }
   
 }
+
